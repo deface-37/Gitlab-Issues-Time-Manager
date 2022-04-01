dd4e32554b5c802b64072783008cc525ff3d7fe1
deface-37 <hause966@mail.ru>
2022-04-02 01:06:00 +0300

    Рефакторинг кнопки обновления всех данных

--- src/components/header/update-button.ts
+++ src/components/header/update-button.ts
@@ -1,59 +1,29 @@
-import { LitElement, html, css } from 'lit';
-import { customElement, query } from 'lit/decorators.js';
+import { LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
 
 import '@spectrum-web-components/action-button/sp-action-button.js';
 import '@spectrum-web-components/tooltip/sp-tooltip.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-data-refresh.js';
 import '@spectrum-web-components/action-menu/sp-action-menu.js';
-import { Overlay } from '@spectrum-web-components/overlay';
+import '@spectrum-web-components/overlay/overlay-trigger.js';
 import { REFETCH_ALL } from '../../eventNames';
 
 @customElement('update-button')
 export class UpdateButton extends LitElement {
-  static styles = [
-    css`
-      :host {
-        display: block;
-      }
-
-      sp-tooltip:not([open]) {
-        display: none;
-      }
-    `,
-  ];
-
-  @query('sp-tooltip')
-  private tooltip: HTMLElement;
-  private overlayClose: Promise<() => void>;
-
   render() {
     return html`
-      <sp-action-button
-        quiet
-        @click=${this._clickHandler}
-        @mouseover=${this._mouseOverHandler}
-        @mouseout=${this._mouseOutHandler}
-      >
-        <sp-icon-data-refresh slot="icon"></sp-icon-data-refresh>
-      </sp-action-button>
-      <sp-tooltip>Обновить данные</sp-tooltip>
+      <overlay-trigger offset="3">
+        <sp-action-button slot="trigger" quiet @click=${this._clickHandler}>
+          <sp-icon-data-refresh slot="icon"></sp-icon-data-refresh>
+        </sp-action-button>
+        <sp-tooltip slot="hover-content" placement="bottom" open delayed
+          >Обновить данные</sp-tooltip
+        >
+      </overlay-trigger>
     `;
   }
 
   private _clickHandler() {
     document.dispatchEvent(new CustomEvent(REFETCH_ALL));
-  }
-
-  private _mouseOverHandler() {
-    this.overlayClose = Overlay.open(this, 'hover', this.tooltip, {
-      placement: 'bottom',
-      delayed: true,
-    });
-  }
-
-  private _mouseOutHandler() {
-    this.overlayClose?.then((close) => {
-      close();
-    });
   }
 }
