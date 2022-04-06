de6ec3fe385f55d1071a81a769cfe05e047690d8
deface-37 <hause966@mail.ru>
2022-04-06 22:00:08 +0300

    Убрано пересоздание apollo-client Убрана зависимость от @apollo-elements/components

---
+++ src/apollo/controllerWithClient.ts
@@ -0,0 +1,13 @@
+import { ApolloQueryController, ApolloQueryControllerOptions } from '@apollo-elements/core';
+import { client } from './client';
+import { ComponentDocument, VariablesOf } from '@apollo-elements/core/types';
+import { ReactiveControllerHost } from 'lit';
+
+export const queryControllerWithClient = <D, V = VariablesOf<D>>(
+  host: ReactiveControllerHost,
+  query: ComponentDocument<D>,
+  options?: ApolloQueryControllerOptions<D, V>
+) => {
+  const currentOptions = Object.assign({ client }, options);
+  return new ApolloQueryController(host, query, currentOptions);
+};
