d2e4b5acf39f85103494fbdded7d996a66b77526
Илья Малышев <hause966@mail.ru>
2023-12-12 00:38:27 +0300

    build: добавлена проверка корректности типов

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [checker({ typescript: true })],
+});
+
