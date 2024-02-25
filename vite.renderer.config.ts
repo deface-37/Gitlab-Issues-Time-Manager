3a7b113281446593bd0d9958af0c630391f6ce9a
Илья Малышев <hause966@mail.ru>
2024-02-25 16:19:19 +0300

    ci: обновил версию electron, electron-forge

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,8 +1,26 @@
+import type { ConfigEnv, UserConfig } from 'vite';
 import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
+import { pluginExposeRenderer } from './vite.base.config';
+import { checker } from 'vite-plugin-checker';
 
 // https://vitejs.dev/config
-export default defineConfig({
-  plugins: [checker({ typescript: true })],
+export default defineConfig((env) => {
+  const forgeEnv = env as ConfigEnv<'renderer'>;
+  const { root, mode, forgeConfigSelf } = forgeEnv;
+  const name = forgeConfigSelf.name ?? '';
+
+  return {
+    root,
+    mode,
+    base: './',
+    build: {
+      outDir: `.vite/renderer/${name}`,
+    },
+    plugins: [pluginExposeRenderer(name), checker({ typescript: true })],
+    resolve: {
+      preserveSymlinks: true,
+    },
+    clearScreen: false,
+  } as UserConfig;
 });
 
